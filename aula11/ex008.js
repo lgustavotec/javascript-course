8169f7453ce1ca6b8bdf0defe6579ec6e60f5342
Lgustavo-github <102495166+Lgustavo-github@users.noreply.github.com>
2022-04-26 17:45:30 +0100

    Desafio JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel =  50
+console.log(`a Velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`diriga sempre usando sinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('você é Estrangeiro!')
+} else {
+    console.log('Você é Brasileiro!')
+}
