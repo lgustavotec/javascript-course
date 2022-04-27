8d6b213dd89e481536f5799797dd243e089a34a0
Lgustavo-github <102495166+Lgustavo-github@users.noreply.github.com>
2022-04-27 22:37:57 +0100

    ex011 e ex012 finalizado.

---
+++ aula12/ex011.js
@@ -0,0 +1,7 @@
+var kg = 30
+console.log(`Sua mala está pesando ${kg} kg`)
+if (kg > 23) {
+    console.log('Excesso de peso. Você nao pode levar!')
+} else if (kg < 23) {
+    console.log('Está tudo ok. Boa Viagem!!!')
+}
