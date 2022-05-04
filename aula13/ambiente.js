a9673e7ff6bf0d9b1e6b380a68a73e7ecda72f85
Lgustavo-github <102495166+Lgustavo-github@users.noreply.github.com>
2022-05-04 12:13:49 +0100

    while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+
+var contador = 1
+do {
+    console.log(`olá numero ${contador}`)
+} while (c <= 6)
+
+
+
+
+
+
+/*
+var contador = 1
+while (contador <= 10) {
+    console.log(`olà numero ${contador}`)
+    contador++ // c = c + 1
+}
+*/
+
+
+
+
+
+/*console.log ('tudo bem?')
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+*/
